27ff56cc758269b2aaae3c2f44b637d85970dd45
ThisIsNishat <Nishat8704@gmail.com>
2025-02-28 16:56:29 +0000

    Lab 5 Completed :)

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,37 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    if n < 2:
+        return "*" * n 
+    
+    square = []
+
+    square.append("*" * n)
+    
+    for _ in range(n - 2):
+        square.append("*" + " " * (n - 2) + "*")
+    
+    square.append("*" * n)
+    
+    return "\n".join(square)
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+   lines = []
+
+   for i in range (1, n + 1):
+      line = ''.join(str(x) for x in range(1, i + 1 )) 
+      lines.append(line)
+
+   return '\n'.join(lines)
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    return n * (n + 1)/2
 
 # Example for n = 4:
 #    *
@@ -24,4 +43,11 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    lines = []  
+  
+    for i in range(1, n + 1):
+        spaces = " " * (n - i)        
+        stars = "*" * (2 * i - 1)   
+        lines.append(spaces + stars)       
+    
+    return "\n".join(lines)
